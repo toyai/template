dd3947e91d67346ecf69bee9f4be8c0bd6c0232c
ydcjeff <ydcjeff@outlook.com>
2020-12-11 15:57:13 +0630

    Init

---
+++ scripts/run_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -xu
+
+if [[ "${TORCH_UTILS_COLLECT_ENV}" -eq 1 ]]; then
+    python -m torch.utils.collect_env
+fi
+
+coverage run --source toydet -m unittest discover -bv -s ./tests/ -p "test_*.py" && coverage report
